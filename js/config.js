b3c2170e7ac719d58afb66ccccbe47a5b60d371e
Ashley <ashleynoelleglidden@gmail.com>
2014-10-27 11:23:38 -0400

    Delete config.js

--- js/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(app) {
-	app.secrets = {
-	  clientId : 'SDSGM5KCYH2APPOD1GDDA5NT312Q1BOZY5FQYJDVVEDYUBY2',
-	  clientSecret : '5GK2Q4GFOI0W3KQYEKM1KYKGJKJIRDIN0RAH5OI1WU4CRIQS',
-	 	redirectUrl : 'http://localhost:8080/callback'
-	}
-};
