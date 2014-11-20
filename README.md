4762b302f92089b647339f314f9547f9dc8f914f
Max Wofford <max.speed.wofford@gmail.com>
2014-11-19 23:45:06 -0600

    revert to commit f96fe090355f20fe8bcc0be54772d0155ae54e3d

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-#Foursquare Search App
 A node.js app that uses the Foursquare explore and Google Maps APIs. 
 Created for Hacker Schoolers (many of whom are new to NYC) to locate food and other necessities for coding. 
 
@@ -11,43 +10,3 @@ complete welcome page
 update markers with more information and design, close automatically
 <br>
 beautify it further
-
-##Dependencies
-* node
-* npm
-
-##Installation
-
-
-Clone & enter: 
-
-```bash
-$git clone https://github.com/ashleyglidden/fsSearchApp.git && cd fsSearchApp/
-```
-
-Install dependencies: 
-
-```bash
-$npm install
-```
-
-Add API keys to config.js:
-```bash 
-$touch js/config.js
-```
-```js
-//config.js
-
-module.exports.foursquare = {
-	// Foursquare API keys in development
-   'clientId' : 'YOUR_CLIENT_ID',
-   'clientSecret' : 'YOUR_CLIENT_SECRET'
-}
-
-```
-
-Run:
-
-```bash
-$node js/
-```
--- js/search.js
+++ js/search.js
@@ -1,18 +1,14 @@
-module.exports = function(app, query, callback) {
-
+module.exports = function(app, searchQuery, callback) {
+  
 	var http = require('http');
   	var https = require('https');
-	var secrets = require('./config');
-	// We'll get our API keys from a config file if we can't find an 'FS_CLIENT_ID' environmental variable
-	var foursquareClientId = process.env.FS_CLIENT_ID || secrets.foursquare.clientId;
-	var foursquareClientSecret = process.env.FS_CLIENT_SECRET || secrets.foursquare.clientSecret;
 
 	var options = {
 		host: 'api.foursquare.com',
 		port: 443,
-
-		path: '/v2/venues/explore?client_id=' + foursquareClientId + '&client_secret=' + 
-			foursquareClientSecret + '&v=20141021&ll=40.72078,-74.001119&query=' + query,
+		path: '/v2/venues/explore?client_id=' + process.env.FS_CLIENT_ID + '&client_secret=' + 
+			process.env.FS_CLIENT_SECRET + '&v=20141021&ll=' + searchQuery.lat + ',' + searchQuery.lng + 
+		'&query=' + searchQuery.query,
 		method: 'GET',
 		headers: {
 			'Content-Type': 'application/json'
@@ -27,8 +23,7 @@ module.exports = function(app, query, callback) {
 				var body = Buffer.concat(dataChunks);
 				var stringBody = body.toString('utf-8');
 				var parsedData = JSON.parse(stringBody);
-			// Log our API call
-	        console.log(options.host + options.path);
+	        //console.log(options.host, options.path);
 	    	callback(null, parsedData);
 			});
 		});
